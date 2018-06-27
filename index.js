af5551396815ab74cb94978be8f277788b479949
huhwhy <dbosah@buffalo.edu>
2018-06-26 20:50:46 -0400

    adds fucntionality to the code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("it works")
 console.warn("omar coming")
 console.error()
 const button = document.querySelector("button1")
+const form = document.querySelector('form#flickform')
 
 
 
@@ -22,13 +23,34 @@ const change2 = function(FormInput) {
  }
 
 
-const change3 = function(Input)  {
+const change3 = function(event)  {
+   
+   event.preventDefault()
 
-   const q = document.querySelector("h3")
-   q.textContent = Input
+   const f = event.target // look up target ( maybe form?)
+
+   const flickName = f.flickName.value //raw name
+   const flickYear = f.flickYear.value // raw year
+	const list = document.querySelector('#flicks') // empty list of #flicks
+
+   const name = document.createElement("span") // creates a span object ( used to group inline elements in a doc)
+   const br = document.createElement("br") // creates a break in page
+   const year = document.createElement("span") // refer to first variable
+   name.textContent = flickName + ' ' // flickName needs space in order to concat and account for adding to list
+   name.className = "Cinema" // assigning the name of the class
+
+   list.appendChild(name) // appends span object to list
+	
+   year.textContent = flickYear // getting text content of the year 
+   year.className = "CinemaYear" // assigning the name of the class
+
+   list.appendChild(year) // appends year to list
+   list.appendChild(br) // appends break object to list
+ 
+   f.reset() // empties the boxes you hit reset on
 }
 
 
 
 
-//button.addEventListener('click', change())
+form.addEventListener('submit', change3)
